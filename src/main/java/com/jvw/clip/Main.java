0ee2c955672783c2a43e1d0b381ef0f54ea68214
Joris van Winden <jorisvanwinden@me.com>
2014-04-16 21:00:36 +0200

    Setup Main activity and layout
    Ip address is pre-configured, as is port number.

--- src/main/java/com/jvw/clip/Main.java
+++ src/main/java/com/jvw/clip/Main.java
@@ -1,38 +1,62 @@
 package com.jvw.clip;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 
-public class Main extends ActionBarActivity {
+public class Main extends ActionBarActivity implements View.OnClickListener {
+
+	private ClipboardManager clipBoard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+	    Button send = (Button) findViewById(R.id.clip_send_button);
+	    clipBoard = (ClipboardManager) getSystemService(Activity.CLIPBOARD_SERVICE);
+	    send.setOnClickListener(this);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public void onClick(View v) {
+		new SendClipboardTask().execute();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private class SendClipboardTask extends AsyncTask<Void, Void, Void> {
 
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				if (clipBoard.hasPrimaryClip()) {
+					if (clipBoard.getPrimaryClip().getItemCount() > 0) {
+						ClipData.Item item = clipBoard.getPrimaryClip().getItemAt(0);
+						String s = item.getText().toString();
+						send(s);
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		private void send(String s) throws IOException {
+			Socket socket = new Socket("192.168.1.39", 60607);
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			out.writeUTF(s);
+			out.flush();
+			out.close();
+		}
+	}
 }
