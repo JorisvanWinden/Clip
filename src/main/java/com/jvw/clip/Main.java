459581327e95cd751239d4d36f71a0b0f0299e3f
Joris van Winden <jorisvanwinden@me.com>
2014-04-17 08:39:30 +0200

    Working on spinner for destination selection
    Added spinner in layout, added menu with button to add pc

--- src/main/java/com/jvw/clip/Main.java
+++ src/main/java/com/jvw/clip/Main.java
@@ -6,8 +6,13 @@ import android.content.ClipboardManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -22,11 +27,32 @@ public class Main extends ActionBarActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 	    Button send = (Button) findViewById(R.id.clip_send_button);
+	    Spinner dest = (Spinner) findViewById(R.id.clip_dest_spinner);
+	    ArrayAdapter<String> spinnerData = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
 	    clipBoard = (ClipboardManager) getSystemService(Activity.CLIPBOARD_SERVICE);
+
+	    spinnerData.add("192.168.1.39");
 	    send.setOnClickListener(this);
     }
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.clip_add_menu:
+				Toast.makeText(this, "Add button clicked", Toast.LENGTH_SHORT).show();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 
 	@Override
 	public void onClick(View v) {
